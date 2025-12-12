7e4c710dadbc222843b530aea09fff2315cbc555
Adair Gustavo <adairgmh@gmail.com>
2025-12-11 23:40:40 -0600

    Actualizar configuración de producción en Django para incluir SERVER_IP y ajustar CSRF_TRUSTED_ORIGINS

--- taller_aws/settings/prod.py
+++ taller_aws/settings/prod.py
@@ -7,35 +7,42 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("Falta DJANGO_SECRET_KEY en producción")
 
+SERVER_IP = os.environ.get("SERVER_IP")
+
 ALLOWED_HOSTS = [
-    os.environ.get("SERVER_IP"), 
+    SERVER_IP,
     "localhost",
     "127.0.0.1",
-]
+] if SERVER_IP else ["localhost", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = [
-    f"http://{os.environ.get('SERVER_IP')}",
-    f"https://{os.environ.get('SERVER_IP')}",
-]
+if SERVER_IP:
+    CSRF_TRUSTED_ORIGINS = [
+        f"http://{SERVER_IP}",
+        f"https://{SERVER_IP}",
+    ]
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 SECURE_HSTS_SECONDS = 0
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASS"),
+#         "HOST": os.environ.get("DB_HOST", "localhost"),
+#         "PORT": os.environ.get("DB_PORT", "5432"),
+#     }
+# }
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
